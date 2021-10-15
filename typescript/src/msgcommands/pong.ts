c274f5ab13ef7da6a8de247814f317f4cfb75047
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-10-16 03:20:38 +0900

    ping -> pong으로 교체

--- typescript/src/msgcommands/pong.ts
+++ typescript/src/msgcommands/pong.ts
@@ -1,23 +1,23 @@
 import { client } from "..";
 import { MsgCommand as Command } from "../interfaces/Command";
-import { I, D, M } from "../aliases/discord.js";
+import { I, D, M } from "../aliases/discord.js.js";
 import { MessageActionRow, MessageButton, MessageEmbed } from "discord.js";
 
-/** 핑 명령어 */
-export default class PingCommand implements Command {
+/** 퐁 명령어 */
+export default class PongCommand implements Command {
   /** 해당 명령어 설명 */
   metadata = {
-    name: 'ping',
-    description: 'Ping!',
-    aliases: ['핑']
+    name: 'pong',
+    description: 'Pong!',
+    aliases: ['퐁']
   };
 
   /** 실행되는 부분 */
-  async run(message: M) {
+  async run(message: M, args: string[]) {
     message.channel.send({
       embeds: [
         new MessageEmbed()
-          .setTitle(`Pong!`)
+          .setTitle(`Ping!`)
           .setDescription(`**${client.ws.ping}ms**`)
           .setFooter(`이 메세지는 곧 삭제됩니다.`)
           .setColor('ORANGE')
