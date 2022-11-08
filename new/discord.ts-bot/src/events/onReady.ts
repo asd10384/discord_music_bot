cd8888033542b4ffcad6df8bbb396c284156a6ec
tkrmagid <tmdgks0466@naver.com>
2022-11-09 00:28:16 +0900

    오류수정

--- new/discord.ts-bot/src/events/onReady.ts
+++ new/discord.ts-bot/src/events/onReady.ts
@@ -1,12 +1,14 @@
+import "dotenv/config";
 import { client, handler } from "..";
 import { Logger } from "../utils/Logger";
 
 export const onReady = () => {
   if (!client.user) return;
-
-  let actlist: { text: string, time: number }[] = JSON.parse(JSON.stringify(process.env.ACTIVITY || '[{ "text": `/help`, time: 10 }, { "text": `${client.prefix}help`, "time": 10 }]'));
+  const prefix = client.prefix;
+  let actlist: { text: string, time: number }[] = eval(process.env.ACTIVITY || '[{ "text": `/help`, time: 10 }, { "text": `${prefix}help`, "time": 10 }]');
 
   Logger.log(`Ready! ${client.user.username}`);
+  Logger.log(`prefix: ${prefix}`);
   Logger.log(`Activity: ${JSON.stringify(actlist)}`);
   Logger.log(`로그확인: ${client.debug}`);
 
